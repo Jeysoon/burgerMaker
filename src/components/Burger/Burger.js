54ad9128d5e571bf934492db213b4300efab6300
Jeyson Mg <jeysoon@gmail.com>
2019-06-25 03:30:43 -0600

    Build controls added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,20 @@ import classes from './Burger.css';
 
 const burger = (props) => {
     //Transforming an object into an array
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey =>{
         //Transform this string value in to an array
         return [...Array(props.ingredients[igKey])]
         .map((_,i) => {
             return  <BurgerIngredient key={igKey + i} type={igKey} />
         }); //[,]
-    });
+    })
+    .reduce((arr, el)=>{
+        return arr.concat(el)
+    },[]);
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React , { Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 class BurgerBuilder extends Component{
     // constructor(props){
     //     super(props);
@@ -8,10 +9,10 @@ class BurgerBuilder extends Component{
     // }
     state = {
         ingredients: {
-            salad: 3,
-            bacon: 2,
-            cheese: 1,
-            meat: 1,
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
         }
     }
 
@@ -20,7 +21,9 @@ class BurgerBuilder extends Component{
     return(
         <Aux>   
             <Burger ingredients={this.state.ingredients} />
-            <div>Buid Control</div>
+            <BuildControls />
+            <div>
+            </div>
         </Aux>    
         );
     }
