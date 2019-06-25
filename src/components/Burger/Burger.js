01024eb1ffb1efe44457ff4f9d9000a1e6dc9177
Jeyson Mg <jeysoon@gmail.com>
2019-06-24 19:03:39 -0600

    Conditionally rendered ingredients added!

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
+
 const burger = (props) => {
+    //Transforming an object into an array
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey =>{
+        //Transform this string value in to an array
+        return [...Array(props.ingredients[igKey])]
+        .map((_,i) => {
+            return  <BurgerIngredient key={igKey + i} type={igKey} />
+        }); //[,]
+    });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,24 @@ import React , { Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component{
-    render() {
-        return (
+    // constructor(props){
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 3,
+            bacon: 2,
+            cheese: 1,
+            meat: 1,
+        }
+    }
+
+
+    render() {    
+    return(
         <Aux>   
-            <Burger />
+            <Burger ingredients={this.state.ingredients} />
             <div>Buid Control</div>
         </Aux>    
         );
