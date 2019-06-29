41e75ffa6bfe94405f7b0c007dce14de25522bc5
Jeyson Mg <jeysoon@gmail.com>
2019-06-29 16:44:39 -0600

    Modal and Backdrop working

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,7 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
-
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 const INGREDIENT_PRICES={
     salad:0.5,
     cheese: 0.4,
@@ -24,7 +24,8 @@ class BurgerBuilder extends Component{
             meat: 0,
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
     updatePurchareState (ingredients){
         // const ingredients = {
@@ -60,7 +61,13 @@ class BurgerBuilder extends Component{
 
 
     };
-    removeIngredientHandler = (type) => {
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    };
+    purchaseHandler = () =>{
+        this.setState({purchasing: true});
+    };
+    removeIngredientHandl1er = (type) => {
         const oldCount = this.state.ingredients[type];
         console.log('[BurguerBuilder.js] => Before oldCount if');
         if(oldCount <= 0){
@@ -90,7 +97,12 @@ class BurgerBuilder extends Component{
         }
     return(
         <Aux>   
-            <Modal />
+            <Modal 
+            show={this.state.purchasing}
+            modalClosed={this.purchaseCancelHandler}>
+                <OrderSummary 
+                ingredients={this.state.ingredients} />
+            </Modal>
             <Burger ingredients={this.state.ingredients} />
             <BuildControls 
             click={this.addIngredient} 
@@ -99,6 +111,7 @@ class BurgerBuilder extends Component{
             disabled={disabledInfo}
             purchasable={this.state.purchasable}
             price={this.state.totalPrice}
+            ordered={this.purchaseHandler}
             />
             <div>
             </div>
